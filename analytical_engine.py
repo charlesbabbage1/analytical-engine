89ff1d9c2c6f570be4aa23a6d3d540d260f825a1
Ada Lovelace <adalovelace121015@gmail.com>
2015-09-23 01:51:51 -0400

    Start of function to compute Bernoulli Numbers

--- analytical_engine.py
+++ analytical_engine.py
@@ -3,3 +3,7 @@ class AnalyticalEngine:
         self.memory= []
         self.punchcard = [[00], [00], [00]]
         self.alu = {}
+
+    # TODO: implement algorithm for computing Bernoulli numbers
+    def compute_bernoulli_numbers():
+        return
